08344146060f9b01c49120fe43035a83b1a2d2e7
Keshanth Jude <keshanth64@gmail.com>
2022-12-03 11:41:52 +0000

    Added Multistep form custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { ReactElement } from "react";
+import { useMultistepForm } from "./useMultiStepForm";
+
 const App: React.FC = () => {
-  return (<>
-    Hello World
-  </>);
+  const steps: ReactElement[] = [
+    <div>Step 1</div>,
+    <div>Step 2</div>,
+    <div>Step 3</div>
+  ];
+  const { currStepIdx, step, nextStep, prevStep, goToStep } = useMultistepForm(steps)
+  const formSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // console.log('Form submitted');
+  }
+
+  return (
+    <div>
+      <form onSubmit={formSubmitHandler}>
+        {currStepIdx + 1}/{steps.length}
+
+        {step}
+
+        {currStepIdx > 0 && <button onClick={prevStep}>Prev</button>}
+        {currStepIdx < steps.length - 1 && <button onClick={nextStep}>Next</button>}
+        {/* TODO: Implement conditions for GoTo Button */}
+      </form>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/useMultistepForm.tsx
@@ -0,0 +1,18 @@
+import { ReactElement, useState } from 'react'
+
+// Custom Hook to handle multi-step forms
+export const useMultistepForm = (steps: ReactElement[]) => {
+    const [currStepIdx, setCurrStepIdx] = useState(0);
+
+    const nextStep = () => setCurrStepIdx(i => (i >= steps.length - 1) ? i : i + 1);
+    const prevStep = () => setCurrStepIdx(i => (i <= 0) ? i : i - 1);
+    const goToStep = (stepIdx: number) => setCurrStepIdx(stepIdx);
+
+    return {
+        currStepIdx, // Current step index
+        step: steps[currStepIdx], // Current step
+        nextStep, // Function to go to next step
+        prevStep, // Function to go to previous step
+        goToStep // Function to go to a specific step
+    }
+}
