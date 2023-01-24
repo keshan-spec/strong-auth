a81c2cf2ac4ad79d53bae85eedc988261659b749
Keshanth Jude <keshanth64@gmail.com>
2023-01-24 17:53:38 +0000

    Contact form issue

--- src/components/ContactForm/ContactDetailsForm.tsx
+++ src/components/ContactForm/ContactDetailsForm.tsx
@@ -28,7 +28,7 @@ type APIResponse = {
 }
 
 export const ContactDetailsForm: React.FC<FormProps> = ({ address, city, country, mobileCountryCode, mobileNo, postalCode, state, updateField }) => {
-    const [countryList, setCountryList] = useState<APIResponse[]>([])
+    const [countryList, setCountryList] = useState<APIResponse[]>()
     const [countryCode, setCountryCode] = useState('')
 
     useEffect(() => {
@@ -44,12 +44,12 @@ export const ContactDetailsForm: React.FC<FormProps> = ({ address, city, country
         <FormWrapperHOC title="Contact details">
             <div>
                 <label >Country</label>
-                <ReactSelect
+                {/* <ReactSelect
                     required
                     options={countryList}
                     value={countryList.find(option => option.value == country) || null}
                     onChange={handleCountryChange}
-                />
+                /> */}
             </div>
             <div>
                 <label htmlFor="mobileNo">Mobile No</label>
